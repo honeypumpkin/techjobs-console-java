3cdeca9c3bdf7bba3702b257d4934c8ca603412a
honeypumpkin <robin.avila88@gmail.com>
2017-07-15 08:46:50 -0500

    Fixed #1 cleaned up comments

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -16,19 +16,9 @@ public class JobData {
 
     private static final String DATA_FILE = "resources/job_data.csv";
     private static Boolean isDataLoaded = false;
-
     private static ArrayList<HashMap<String, String>> allJobs;
-
-    /**
-     * Fetch list of all values from loaded data,
-     * without duplicates, for a given column.
-     *
-     * @param field The column to retrieve values from
-     * @return List of all of the values of the given field
-     */
     public static ArrayList<String> findAll(String field) {
 
-        // load data, if not already loaded
         loadData();
 
         ArrayList<String> values = new ArrayList<>();
@@ -44,42 +34,17 @@ public class JobData {
         return values;
     }
 
+
     public static ArrayList<HashMap<String, String>> findAll() {
 
-        // load data, if not already loaded
         loadData();
 
         return allJobs;
     }
 
 
-    /**
-     * Returns results of search the jobs data by key/value, using
-     * inclusion of the search term.
-     * <p>
-     * For example, searching for employer "Enterprise" will include results
-     * with "Enterprise Holdings, Inc".
-     *
-    // * @param column Column that should be searched.
-    // * @param value  Value of the field to search for
-    // * @return List of all jobs matching the criteria
-     */
-
-
-    /* TODO
-    In the JobData class, create a new (public static) method that will search for a String within each of the columns. Name it findByValue. Here are a few observations:
-
-    1. The method that you write should not contain duplicate jobs. So, for example, if a listing has position type "Web - Front End" and name "Front end web dev" then searching for "web" should not include the listing twice.
-    2. As with printJobs, you should write your code in a way that if a new column is added to the data, your code will automatically search the new column as well.
-    3. You should not write code that calls findByColumnAndValue once for each column. Rather, utilize loops and collection methods as you did above.
-    4. You should, on the other hand, read and understand findByColumnAndValue, since your code will look similar in some ways.
-
-    You'll need to call findByValue from somewhere in main. We'll leave it up to you to find where. You might have noticed that when you try to search all columns using the app, a message is printed, so that is a good clue to help you find where to place this new method call. Once you find where to call your new method, you can Run the program again to test your code.
-
-     */
     public static ArrayList<HashMap<String, String>> findByValue(String value) {
 
-        // load data, if not already loaded
         loadData();
 
         ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
@@ -97,21 +62,8 @@ public class JobData {
         return jobs;
     }
 
-
-
-
-
-
-//      for (HashMap<String, String> entry : someJobs) {
-//        System.out.println("*****");
-//        for (String key : entry.keySet()) {
-//            String value = entry.get(key);
-//            System.out.println(key + ": " + value);
-
-
     public static ArrayList<HashMap<String, String>> findByColumnAndValue(String column, String value) {
 
-        // load data, if not already loaded
         loadData();
 
         ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
@@ -128,19 +80,15 @@ public class JobData {
         return jobs;
     }
 
-    /**
-     * Read in data from a CSV file and store it in a list
-     */
+
     private static void loadData() {
 
-        // Only load data once
         if (isDataLoaded) {
             return;
         }
 
         try {
 
-            // Open the CSV file and set up pull out column header info and records
             Reader in = new FileReader(DATA_FILE);
             CSVParser parser = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(in);
             List<CSVRecord> records = parser.getRecords();
@@ -149,7 +97,6 @@ public class JobData {
 
             allJobs = new ArrayList<>();
 
-            // Put the records into a more friendly format
             for (CSVRecord record : records) {
                 HashMap<String, String> newJob = new HashMap<>();
 
@@ -160,7 +107,6 @@ public class JobData {
                 allJobs.add(newJob);
             }
 
-            // flag the data as loaded, so we don't do it twice
             isDataLoaded = true;
 
         } catch (IOException e) {
